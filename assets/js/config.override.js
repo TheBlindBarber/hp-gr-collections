1a2a0f486cd48e6c633ed6fa75ce3d04fb82a0d6
The Blind Barber <n.panagiotakis@gmail.com>
2025-11-25 23:21:57 +0200

    Update config.override.js

--- assets/js/config.override.js
+++ assets/js/config.override.js
@@ -6,3 +6,10 @@ window.siteConfig.occurrence = {
     value: "6e73ce4f-628d-4317-8895-3ddb0d154c0b"
   }
 };
+
+window.loadOccurrenceData = async function() {
+    const response = await fetch("http://YOURSERVER_IP:9090/occurrence.json");
+    const data = await response.json();
+    console.log("Loaded", data.length, "records");
+    return data;
+};
